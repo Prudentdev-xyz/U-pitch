5e64f480b37c9f525c678545f01f6aafe705e5b3
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-03-23 18:03:10 -0700

    header-setup1-fix

---
+++ src/layouts/header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const links = [
+  {
+    link: "",
+    text: "Live",
+  },
+
+  {
+    link: "",
+    text: "Fixtures",
+  },
+
+  {
+    links: "",
+    text: "Standings",
+  },
+  {
+    link: "",
+    text: "Leaderboards",
+  },
+  {
+    link: "",
+    text: "News",
+  },
+];
+
+export const header = () => {
+  return <div>header</div>;
+};
