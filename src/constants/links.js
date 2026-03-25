fb7cabb40ae2e152236cbebc7be65a8608655717
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-03-25 15:51:25 -0700

    header-setup2-fix

---
+++ src/constants/links.js
@@ -0,0 +1,24 @@
+export const links = [
+  {
+    link: "/",
+    text: "Live",
+  },
+
+  {
+    link: "",
+    text: "Fixtures",
+  },
+
+  {
+    link: "",
+    text: "Standings",
+  },
+  {
+    link: "",
+    text: "Leaderboards",
+  },
+  {
+    link: "",
+    text: "News",
+  },
+];
--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,30 +1,19 @@
-import React from "react";
+import React from 'react'
+import logo from "../assets/logo.svg";
+import { links } from '../constants/links';
 
-export const links = [
-  {
-    link: "",
-    text: "Live",
-  },
 
-  {
-    link: "",
-    text: "Fixtures",
-  },
 
-  {
-    links: "",
-    text: "Standings",
-  },
-  {
-    link: "",
-    text: "Leaderboards",
-  },
-  {
-    link: "",
-    text: "News",
-  },
-];
-
-export const header = () => {
-  return <div>header</div>;
+export const Header = () => {
+  return (
+    <>
+    <div className='flex justify-between items-center max-w-screen-2 py-4 px-9 w-full bg-background z-20 border-ghost-white'>
+      <div className='h-12'>
+        
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+    </>
+  );
 };
+
